c9a1b61eacbcc72e781875855e244b0748283cd7
Lutro <stevengamez00@gmail.com>
2021-08-12 10:07:31 -0700

    First version of application moved into GitHub

---
+++ models/publisher.js
@@ -0,0 +1,21 @@
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var PublisherSchema = new Schema(
+  { 
+    name: {type: String, required: true, minlength: 10, maxlength: 60}
+  }
+);
+
+// Virtual for book instance's URL
+PublisherSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/publisher/' + this._id;
+});
+
+
+//Export model
+module.exports = mongoose.model('Publisher', PublisherSchema);
